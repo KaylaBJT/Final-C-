88fef05631eb0eb232166859edfd9fbcc733f92a
Lizaveta <drago719@mail.ru>
2023-01-19 00:43:49 +0300

    final tasks

---
+++ FinC#64/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+void PrintIntegers(int N)
+{
+    if (N <= 1)
+    {
+        Console.WriteLine(N);
+        return;
+    }
+    Console.Write($"{N}, ");
+    PrintIntegers(N - 1);
+}
+
+PrintIntegers(7);
---
+++ FinC#66/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+void SumInteger(int m, int n, int sum)
+{
+    sum += n;
+    if (n <= m)
+    {
+        System.Console.Write($"{sum}");
+        return;
+    }
+    SumInteger(m, n - 1, sum);
+}
+SumInteger(4, 8, 0);
